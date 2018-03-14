7c8fa20569271ab69ddb8ec303573ab44b9a26b6
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-03-14 23:25:55 +0100

    modif du template

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,9 +3,9 @@
         <h1>Bitsound (version 2.0)</h1>
         <p>Ceci est la page about</p>
         @if(count($services) > 0)
-            <ul>
+            <ul class="list-group">
                 @foreach($services as $service)
-                    <li>{{$service}}</li>
+                    <li class="list-group-item">{{$service}}</li>
                 @endforeach
             </ul>
         @endif
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,5 +1,10 @@
+<?php $chemin = '/laravel/bitsound_v2/public' ;?>
 @extends('layouts.app')
     @section('content')
-        <h1>{{$title}}</h1>
-        <p>Ceci est la page d'accueil</p>
+        <div class="jumbotron text-center">
+            <h1>{{$title}}</h1>
+            <p>Ceci est la page d'accueil</p>
+            <a href="{{$chemin}}/login" class="btn btn-primary">Se connecter</a>
+            <a href="{{$chemin}}/register" class="btn btn-danger">S'enregistrer</a>
+        </div>
     @endsection
