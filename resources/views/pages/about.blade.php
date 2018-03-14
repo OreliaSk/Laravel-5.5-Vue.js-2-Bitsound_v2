6186cfc1aee7874c198d0b30bc3d82da4509aa42
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-03-14 11:49:06 +0100

    travail sur les différentes views

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,13 +1,12 @@
-<!doctype html>
-<html lang="{{ app()->getLocale() }}">
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>{{config('app.name', 'Bitsound (version 2.0)')}}</title>
-    </head>
-    <body>
+@extends('layouts.app')
+    @section('content')
         <h1>Bitsound (version 2.0)</h1>
         <p>Ceci est la page about</p>
-    </body>
-</html>
+        @if(count($services) > 0)
+            <ul>
+                @foreach($services as $service)
+                    <li>{{$service}}</li>
+                @endforeach
+            </ul>
+        @endif
+    @endsection
