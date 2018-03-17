44cfb52f4e3e8c930ca8f945319d7c405da9bd2a
Orelia Sokambi <orelia.sokambi@gmail.com>
2018-03-18 00:07:53 +0100

    ajout migration table pour creation d'artistes

---
+++ database/migrations/2018_03_17_192848_create_artists_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateArtistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artists', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('genre');
+            $table->mediumText('biography');
+            $table->string('soundcloud');
+            $table->string('youtube');
+            $table->string('picture');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artists');
+    }
+}
